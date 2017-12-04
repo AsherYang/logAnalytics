e29c51925114d16470698ed35d924a197cd8d5d4
AsherYang <ouyangfan1991@gmail.com>
2017-12-04 17:10:09 +0800

    open file and save file

--- LogAnalytics.py
+++ LogAnalytics.py
@@ -8,6 +8,12 @@
 
 from PyQt4 import QtCore, QtGui
 import sys
+# import pickle
+
+reload(sys)
+# print sys.getdefaultencoding()
+sys.setdefaultencoding('utf8')
+# print sys.getdefaultencoding()
 
 try:
     _fromUtf8 = QtCore.QString.fromUtf8
@@ -26,7 +32,7 @@ except AttributeError:
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
         MainWindow.setObjectName(_fromUtf8("MainWindow"))
-        MainWindow.resize(800, 600)
+        # MainWindow.resize(800, 600)
         self.centralwidget = QtGui.QWidget(MainWindow)
         self.centralwidget.setObjectName(_fromUtf8("centralwidget"))
         self.label = QtGui.QLabel(self.centralwidget)
@@ -57,13 +63,94 @@ class Ui_MainWindow(object):
         self.menu_2.setTitle(_translate("MainWindow", "设置", None))
 
 
+class LogMainWindow(QtGui.QMainWindow):
+    def __init__(self, parent=None):
+        QtGui.QMainWindow.__init__(self)
+
+        screen = QtGui.QDesktopWidget().screenGeometry()
+        self.resize(screen.width() / 4 * 3, screen.height() / 4 * 3)
+        self.setWindowTitle('LogAnalytics')
+
+        self.statusBar()
+
+        menuBar = self.menuBar()
+        file = menuBar.addMenu(' &File')
+        openFile1Action = QtGui.QAction('Open file 1', self)
+        openFile1Action.setStatusTip(_fromUtf8('选择文件1'))
+        openFile1Action.connect(openFile1Action, QtCore.SIGNAL('triggered()'), self.openFile1)
+
+        openFile2Action = QtGui.QAction('Open file 2', self)
+        openFile2Action.setStatusTip(_fromUtf8('选择文件2'))
+        openFile2Action.connect(openFile2Action, QtCore.SIGNAL('triggered()'), self.openFile2)
+
+        saveLogFileAction = QtGui.QAction('Save file', self)
+        saveLogFileAction.setStatusTip(_fromUtf8('保存日志分析文件'))
+        saveLogFileAction.setShortcut('Ctrl+Alt+S')
+        saveLogFileAction.connect(saveLogFileAction, QtCore.SIGNAL('triggered()'), self.saveLogFile)
+
+        file.addAction(openFile1Action)
+        file.addAction(openFile2Action)
+        file.addAction(saveLogFileAction)
+
+        setting = menuBar.addMenu('&Setting')
+        tools = menuBar.addMenu('Tools')
+
+        self.textEdit = QtGui.QTextEdit()
+        self.textEdit.setFont(self.getFont('Monospace'))
+        self.setCentralWidget(self.textEdit)
+
+    def getFont(self, fontStr):
+        font = QtGui.QFont()
+        font.setFamily(fontStr)
+        font.setPointSize(10)
+        font.setFixedPitch(True)
+        return font
+
+    def openFile1(self):
+        fileName = unicode(QtGui.QFileDialog.getOpenFileName(self, 'Open file 1', './', 'log files(*.log *.md *.txt)'))
+        if not fileName:
+            return
+        file = open(fileName)
+        data = file.read()
+        file.close()
+        self.textEdit.setText(_translate('', data, None))
+
+    def openFile2(self):
+        fileName = unicode(QtGui.QFileDialog.getOpenFileName(self, 'Open file 2', './', 'log files(*.log *.md *.txt)'))
+        if not fileName:
+            return
+        file = open(fileName)
+        data = file.read()
+        file.close()
+        self.textEdit.setText(_translate('', data, None))
+
+    def saveLogFile(self):
+        fileName = unicode(QtGui.QFileDialog.getSaveFileName(self, 'save File', './'))
+        if not fileName:
+            return
+
+        try:
+            out_file = open(str(fileName), 'wb')
+        except IOError:
+            QtGui.QMessageBox.information(self, "Unable to open file",
+                                          "There was an error opening \"%s\"" % fileName)
+            return
+        text = str(self.textEdit.toPlainText())
+        # pickle.dump(text, out_file)
+        out_file.write(text)
+        out_file.close()
+        # print text
+        print fileName
+
+
 if __name__ == '__main__':
     app = QtGui.QApplication(sys.argv)
     # label = QtGui.QLabel("<center>Hello World with PyQt4!</center>")
     # label.resize(500, 400)
     # label.show()
-    mainWin = QtGui.QMainWindow()
-    uiMainWin = Ui_MainWindow()
-    uiMainWin.setupUi(mainWin)
-    mainWin.show()
+    # mainWin = QtGui.QMainWindow()
+    logMainWin = LogMainWindow()
+    # uiMainWin = Ui_MainWindow()
+    # uiMainWin.setupUi(logMainWin)
+    logMainWin.show()
     sys.exit(app.exec_())
