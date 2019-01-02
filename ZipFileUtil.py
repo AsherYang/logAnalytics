960d1e612be933f6883bdf6362e17e8159ade4e5
AsherYang <ouyangfan1991@gmail.com>
2019-01-02 15:26:18 +0800

    1. 修复Zip 解压缩遇到错误时不能继续解压的问题

--- ZipFileUtil.py
+++ ZipFileUtil.py
@@ -26,34 +26,46 @@ class ZipFileUtil:
         if not src_dir:
             self.doCallBack(u'请输入需要解压缩的路径')
             return
-        allZipFileList = FileUtil.getAllFilesByExt(src_dir, 'zip')
-        allGzFileList = FileUtil.getAllFilesByExt(src_dir, 'gz')
-        # print '--->src_dir:%s allGzFileList:%s ' % (src_dir, allGzFileList)
-        if not allZipFileList and not allGzFileList:
-            logStr = u'在目录 ' + _translateUtf8(src_dir) + u' 及子目录下未找到 zip 文件'
-            self.doCallBack(logStr)
-            return
-        for file_path in allZipFileList:
-            log_txt = u'正在解压文件: ' + _translateUtf8(file_path)
-            self.doCallBack(log_txt)
-            # print str(_translateUtf8(file_path))
-            dest_dir = _translateUtf8(FileUtil.getFilePathWithName(file_path))
-            zip_ref = zipfile.ZipFile(unicode(file_path), 'r')
-            # FileUtil.mkdirNotExist(str(dest_dir)) # zipfile 会自动创建
-            zip_ref.extractall(unicode(dest_dir))
-            zip_ref.close()
-            self.recursiveUnZipFile(dest_dir)
-        for file_path in allGzFileList:
-            log_txt = u'正在解压文件: ' + _translateUtf8(file_path)
-            self.doCallBack(log_txt)
-            dest_dir = str(_translateUtf8(FileUtil.getFilePathWithName(file_path)))
-            if FileUtil.getFileExt(dest_dir).find('tar') != -1:
-                dest_dir = str(_translateUtf8(FileUtil.getFilePathWithName(dest_dir)))
-                # print '----dest_dir: ', dest_dir
-            tarf = tarfile.open(unicode(file_path))
-            tarf.extractall(unicode(dest_dir))
-            tarf.close()
-            self.recursiveUnZipFile(dest_dir)
+        dest_dir = None
+        zip_ref = None
+        tar_ref = None
+        try:
+            allZipFileList = FileUtil.getAllFilesByExt(src_dir, 'zip')
+            allGzFileList = FileUtil.getAllFilesByExt(src_dir, 'gz')
+            # print '--->src_dir:%s allGzFileList:%s ' % (src_dir, allGzFileList)
+            if not allZipFileList and not allGzFileList:
+                logStr = u'在目录 ' + _translateUtf8(src_dir) + u' 及子目录下未找到 zip 文件'
+                self.doCallBack(logStr)
+                return
+            for file_path in allZipFileList:
+                log_txt = u'正在解压文件: ' + _translateUtf8(file_path)
+                self.doCallBack(log_txt)
+                # print str(_translateUtf8(file_path))
+                dest_dir = _translateUtf8(FileUtil.getFilePathWithName(file_path))
+                zip_ref = zipfile.ZipFile(unicode(file_path), 'r')
+                # FileUtil.mkdirNotExist(str(dest_dir)) # zipfile 会自动创建
+                zip_ref.extractall(unicode(dest_dir))
+                zip_ref.close()
+                self.recursiveUnZipFile(dest_dir)
+            for file_path in allGzFileList:
+                log_txt = u'正在解压文件: ' + _translateUtf8(file_path)
+                self.doCallBack(log_txt)
+                dest_dir = str(_translateUtf8(FileUtil.getFilePathWithName(file_path)))
+                if FileUtil.getFileExt(dest_dir).find('tar') != -1:
+                    dest_dir = str(_translateUtf8(FileUtil.getFilePathWithName(dest_dir)))
+                    # print '----dest_dir: ', dest_dir
+                tar_ref = tarfile.open(unicode(file_path))
+                tar_ref.extractall(unicode(dest_dir))
+                tar_ref.close()
+                self.recursiveUnZipFile(dest_dir)
+        except:
+            if dest_dir:
+                self.recursiveUnZipFile(dest_dir)
+        finally:
+            if zip_ref:
+                zip_ref.close()
+            if tar_ref:
+                tar_ref.close()
 
     def doCallBack(self, msg):
         if self.call_back:
