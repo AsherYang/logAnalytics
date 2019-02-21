46c732d68ab47a030dd184e3817f14414b6f7bdb
AsherYang <ouyangfan1991@gmail.com>
2019-02-21 15:47:05 +0800

    add setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='logAnalytics',
+    version='0.0.1',
+    packages=['splitexcel', 'analyticslog'],
+    url='www.oyf.name',
+    license='14434656',
+    author='Asher',
+    author_email='1181830457@qq.com',
+    description='Log analytics'
+)
